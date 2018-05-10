ea41e164d1fe08002b364aa8b4f688a5ce98a14e
Alex Sinclair <Alex@MAINCOMPUTER>
2018-05-10 13:32:10 +0100

    fixed a test

--- test/RuleProviderUnitTests.java
+++ test/RuleProviderUnitTests.java
@@ -1,8 +1,4 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 import java.io.FileNotFoundException;
 import java.util.HashSet;
